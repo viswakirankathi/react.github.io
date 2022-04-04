579e2f6c5de02d8f19580119d4060ce06887795d
viswakirankathi <102529301+viswakirankathi@users.noreply.github.com>
2022-04-04 21:20:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './Components/Header.js';
+
 function App() {
   return (
    <div className='Header'>
-     Hello World
+    <Header/>
    </div>
   );
 }
