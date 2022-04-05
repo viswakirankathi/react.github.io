5cd3db8ae8859958ea70d2e8038afd66825b191c
viswakirankathi <102529301+viswakirankathi@users.noreply.github.com>
2022-04-05 20:08:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,22 +5,7 @@ import Header from './Components/Header.js';
 
 function App() {
   return (
-   <div className='Header'>
-    <Header/>
-  <div className="container">
-    <div className="row">   <div className="col-3 col-centered">
-      <div className="mb-3">
-  <label for="exampleFormControlInput1" className="form-label">Email address</label>
-  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-</div>
-<div className="mb-3">
-  <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-</div></div></div>
-  </div>
- 
-    
-   </div>
+   <div>Hello</div>
   );
 }
 
