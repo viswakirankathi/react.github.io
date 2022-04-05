3b2c7200c9e9f38797d4cd1dcf4fcd4e8ea235e8
viswakirankathi <102529301+viswakirankathi@users.noreply.github.com>
2022-04-05 21:35:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
-import Header from './Components/Header.js';
-
+import Header from './Components/Header';
+import Home from './Components/Home.js';
+import About from './Components/About';
+import {Route, BrowserRouter as Router,Switch ,Link} from 'react-router-dom';
 function App() {
   return (
-   <div>Hello</div>
+    <Router>
+      <div className='Header'></div>
+      <Link to="/menu"><Home/></Link>
+      <Link to="/home">Home</Link>
+     <Switch>
+       <Route path='/menu'>
+         <Header/>
+       </Route>
+       <Route path='/home'>
+         <About/>
+       </Route>
+     </Switch>
+    </Router>
+   
+   
   );
 }
 
---
+++ src/Components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius quas voluptatem magnam veniam, iste maxime officia tempora excepturi nemo recusandae!</div>
+  )
+}
+
+export default About
